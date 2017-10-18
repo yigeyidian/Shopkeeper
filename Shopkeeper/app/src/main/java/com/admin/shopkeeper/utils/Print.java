11ac8ee12999b43ebeaf3afedddbc5d6d9a53db8
yigeyidian <976854315@qq.com>
2017-10-18 16:17:19 +0800

    111

--- Shopkeeper/app/src/main/java/com/admin/shopkeeper/utils/Print.java
+++ Shopkeeper/app/src/main/java/com/admin/shopkeeper/utils/Print.java
@@ -166,11 +166,36 @@ public class Print {
                     if (enty[3].equals("6")) {
                         if (enty[10].equals("0")) {
                             PrintClass58 p = new PrintClass58(enty[0], enty[3], enty[11], enty[1], enty[2], enty[5], enty[6], enty[7], enty[8], enty[9]);
-                            p.Print();
+                            boolean t = p.Print();
+//                            if (t) {
+//                                //修改菜品打印状态
+////                                string SocketType = System.Configuration.ConfigurationManager.AppSettings["SocketType"];
+////                                SendData("A*" + SocketType + "*" + enty[4] + "@1*AS05#");
+//                            }
+                            if (t) {
+                                RetrofitHelper.getInstance()
+                                        .getApi()
+                                        .updatePrint(enty[4])
+                                        .subscribeOn(Schedulers.io())
+                                        .observeOn(AndroidSchedulers.mainThread())
+                                        .subscribe();
+                            }
                         } else {
                             PrintClass p = new PrintClass(enty[0], enty[3], enty[11], enty[1], enty[2], enty[5], enty[6], enty[7], enty[8], enty[9]);
-                            p.Print();
-
+                            boolean t = p.Print();
+//                            if (t) {
+//                                //修改菜品打印状态
+////                                string SocketType = System.Configuration.ConfigurationManager.AppSettings["SocketType"];
+////                                SendData("A*" + SocketType + "*" + enty[4] + "@1*AS05#");
+//                            }
+                            if (t) {
+                                RetrofitHelper.getInstance()
+                                        .getApi()
+                                        .updatePrint(enty[4])
+                                        .subscribeOn(Schedulers.io())
+                                        .observeOn(AndroidSchedulers.mainThread())
+                                        .subscribe();
+                            }
                         }
                     }
                     //排号打印
