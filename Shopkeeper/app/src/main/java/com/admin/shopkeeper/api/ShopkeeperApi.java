2b99d754fccf23fd4ec15b008fb958577054e852
yigeyidian <976854315@qq.com>
2017-11-30 17:29:56 +0800

    111

--- Shopkeeper/app/src/main/java/com/admin/shopkeeper/api/ShopkeeperApi.java
+++ Shopkeeper/app/src/main/java/com/admin/shopkeeper/api/ShopkeeperApi.java
@@ -1811,7 +1811,7 @@ public interface ShopkeeperApi {
             @Field("RESTAURANTID") String shopId,
             @Field("CardID") String cardID,
             @Field("PayType") int payType,
-            @Field("OperaName ") String operaName,
+            @Field("OperaName") String operaName,
             @Field("OperaID") String operaId);
 
     @FormUrlEncoded
--- Shopkeeper/app/src/main/java/com/admin/shopkeeper/utils/PrintClass.java
+++ Shopkeeper/app/src/main/java/com/admin/shopkeeper/utils/PrintClass.java
@@ -238,24 +238,23 @@ public class PrintClass {
             socketwriter.write(bty_tmp, 0, bty_tmp.length);
         }
         //充值打印
-        if (type.equals("10"))
-        {
+        if (type.equals("10")) {
             String[] er = product.split("\\@");
             socketwriter.write(Internal, 0, Internal.length);
-            byte[] temps = new byte[] { 29, 33, 17 };//1D 21 选择字体大小,0x01放大一倍
+            byte[] temps = new byte[]{29, 33, 17};//1D 21 选择字体大小,0x01放大一倍
             socketwriter.write(temps, 0, temps.length);//设置字符集
             byte[] datas11 = ("" + sname.split("\\@")[0] + "" + "\n\n").getBytes("gbk");
             socketwriter.write(datas11, 0, datas11.length);//输出文字
-            byte[] temp8 = new byte[] { 27, 50 };//1D 21 选择字体大小,0x01放大一倍
+            byte[] temp8 = new byte[]{27, 50};//1D 21 选择字体大小,0x01放大一倍
             socketwriter.write(temp8, 0, temp8.length);//设置字符集
-            byte[] temp9 = new byte[] { 29, 33, 0 };//1D 21 选择字体大小,0x01放大一倍
+            byte[] temp9 = new byte[]{29, 33, 0};//1D 21 选择字体大小,0x01放大一倍
             socketwriter.write(temp9, 0, temp9.length);//设置字符集
             byte[] datas31 = ("       会员卡充值凭据        \n").getBytes("gbk");
             socketwriter.write(datas31, 0, datas31.length);//输出文字
             byte[] datas3 = ("-------------------------------\n").getBytes("gbk");
             socketwriter.write(datas3, 0, datas3.length);//输出文字
             socketwriter.write(Internal, 0, Internal.length);
-            byte[] temp = new byte[] { 29, 33, 0 };//1D 21 选择字体大小,0x01放大一倍
+            byte[] temp = new byte[]{29, 33, 0};//1D 21 选择字体大小,0x01放大一倍
             socketwriter.write(temp, 0, temp.length);//设置字符集
             byte[] datas32 = ("操 作 人：" + username + "\n\n").getBytes("gbk");
             socketwriter.write(datas32, 0, datas32.length);//输出文字
